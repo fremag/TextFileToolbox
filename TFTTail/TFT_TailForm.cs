1796c90af2cf4c2a1439180391c9eec07eaed4a4
fremag <fremag.gh@gmail.com>
2017-02-18 14:37:18 +0100

    [Tail] first step : an empty form with WinFwk initialized Fixes #1

--- TFTTail/TFT_TailForm.cs
+++ TFTTail/TFT_TailForm.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using WinFwk.UIModules;
 
 namespace TFT.Tail
@@ -16,6 +8,13 @@ namespace TFT.Tail
         public TFT_TailForm()
         {
             InitializeComponent();
+        }
+
+        private void TFT_TailForm_Load(object sender, EventArgs e)
+        {
+            InitToolBars();
+            InitWorkplace();
+            InitLog();
         }
     }
 }
