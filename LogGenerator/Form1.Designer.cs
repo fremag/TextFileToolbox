346134eab376095b9dc9edaa876bfa3277870587
fremag <fremag.gh@gmail.com>
2017-03-04 14:16:27 +0100

    [tail] Create a dummy log generator to test tail module

---
+++ LogGenerator/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace LogGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.nudPeriod = new System.Windows.Forms.NumericUpDown();
+            this.lblPeriod = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeriod)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(110, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(279, 20);
+            this.tbPath.TabIndex = 0;
+            this.tbPath.Text = "E:\\tmp\\logs\\process_{0:yyyyMMdd_HHmmss}.log";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 15);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(32, 13);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "Path:";
+            // 
+            // nudPeriod
+            // 
+            this.nudPeriod.Location = new System.Drawing.Point(110, 38);
+            this.nudPeriod.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudPeriod.Name = "nudPeriod";
+            this.nudPeriod.Size = new System.Drawing.Size(120, 20);
+            this.nudPeriod.TabIndex = 2;
+            this.nudPeriod.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // lblPeriod
+            // 
+            this.lblPeriod.AutoSize = true;
+            this.lblPeriod.Location = new System.Drawing.Point(12, 40);
+            this.lblPeriod.Name = "lblPeriod";
+            this.lblPeriod.Size = new System.Drawing.Size(62, 13);
+            this.lblPeriod.TabIndex = 3;
+            this.lblPeriod.Text = "Period (ms):";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(256, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 298);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblPeriod);
+            this.Controls.Add(this.nudPeriod);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.tbPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeriod)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.NumericUpDown nudPeriod;
+        private System.Windows.Forms.Label lblPeriod;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LogGenerator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StreamWriter writer;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var fileName = string.Format(tbPath.Text, DateTime.Now);
+            writer = new StreamWriter(fileName);
+            timer1.Tick += LogInfo;
+            timer1.Interval = (int)nudPeriod.Value;
+            timer1.Start();
+        }
+        int n = 0;
+        Random rand = new Random(0);
+        private void LogInfo(object sender, EventArgs e)
+        {
+            Log($"Request sent: {n++}");
+            Thread.Sleep(rand.Next(50));
+            Log($"Reply received: {n}");
+        }
+
+        private void Log(string msg, string type="INFO")
+        {
+            string line = $"{DateTime.Now:HH:mm:ss.fff} - {type} - {msg}";
+            writer.WriteLine(line);
+            writer.Flush();
+
+        }
+    }
+}
