0108e655b8148317d37f019be6925d4be191c8c6
fremag <fremag.gh@gmail.com>
2017-03-05 20:06:58 +0100

    LogGenerator: add error/debug info in the log file

--- LogGenerator/Form1.Designer.cs
+++ LogGenerator/Form1.Designer.cs
@@ -34,8 +34,18 @@
             this.nudPeriod = new System.Windows.Forms.NumericUpDown();
             this.lblPeriod = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerInfo = new System.Windows.Forms.Timer(this.components);
+            this.lblErrorPeriod = new System.Windows.Forms.Label();
+            this.nudErrorPeriod = new System.Windows.Forms.NumericUpDown();
+            this.btnError = new System.Windows.Forms.Button();
+            this.timerError = new System.Windows.Forms.Timer(this.components);
+            this.btnDebug = new System.Windows.Forms.Button();
+            this.lblDebug = new System.Windows.Forms.Label();
+            this.nudDebugPeriod = new System.Windows.Forms.NumericUpDown();
+            this.timerDebug = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.nudPeriod)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudErrorPeriod)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDebugPeriod)).BeginInit();
             this.SuspendLayout();
             // 
             // tbPath
@@ -77,9 +87,9 @@
             this.lblPeriod.AutoSize = true;
             this.lblPeriod.Location = new System.Drawing.Point(12, 40);
             this.lblPeriod.Name = "lblPeriod";
-            this.lblPeriod.Size = new System.Drawing.Size(62, 13);
+            this.lblPeriod.Size = new System.Drawing.Size(83, 13);
             this.lblPeriod.TabIndex = 3;
-            this.lblPeriod.Text = "Period (ms):";
+            this.lblPeriod.Text = "Info Period (ms):";
             // 
             // btnStart
             // 
@@ -91,11 +101,89 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // lblErrorPeriod
+            // 
+            this.lblErrorPeriod.AutoSize = true;
+            this.lblErrorPeriod.Location = new System.Drawing.Point(12, 66);
+            this.lblErrorPeriod.Name = "lblErrorPeriod";
+            this.lblErrorPeriod.Size = new System.Drawing.Size(87, 13);
+            this.lblErrorPeriod.TabIndex = 6;
+            this.lblErrorPeriod.Text = "Error Period (ms):";
+            // 
+            // nudErrorPeriod
+            // 
+            this.nudErrorPeriod.Location = new System.Drawing.Point(110, 64);
+            this.nudErrorPeriod.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudErrorPeriod.Name = "nudErrorPeriod";
+            this.nudErrorPeriod.Size = new System.Drawing.Size(120, 20);
+            this.nudErrorPeriod.TabIndex = 5;
+            this.nudErrorPeriod.Value = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            // 
+            // btnError
+            // 
+            this.btnError.Location = new System.Drawing.Point(256, 61);
+            this.btnError.Name = "btnError";
+            this.btnError.Size = new System.Drawing.Size(75, 23);
+            this.btnError.TabIndex = 7;
+            this.btnError.Text = "Error";
+            this.btnError.UseVisualStyleBackColor = true;
+            this.btnError.Click += new System.EventHandler(this.btnError_Click);
+            // 
+            // btnDebug
+            // 
+            this.btnDebug.Location = new System.Drawing.Point(256, 87);
+            this.btnDebug.Name = "btnDebug";
+            this.btnDebug.Size = new System.Drawing.Size(75, 23);
+            this.btnDebug.TabIndex = 10;
+            this.btnDebug.Text = "Debug";
+            this.btnDebug.UseVisualStyleBackColor = true;
+            this.btnDebug.Click += new System.EventHandler(this.btnDebug_Click);
+            // 
+            // lblDebug
+            // 
+            this.lblDebug.AutoSize = true;
+            this.lblDebug.Location = new System.Drawing.Point(12, 92);
+            this.lblDebug.Name = "lblDebug";
+            this.lblDebug.Size = new System.Drawing.Size(97, 13);
+            this.lblDebug.TabIndex = 9;
+            this.lblDebug.Text = "Debug Period (ms):";
+            // 
+            // nudDebugPeriod
+            // 
+            this.nudDebugPeriod.Location = new System.Drawing.Point(110, 90);
+            this.nudDebugPeriod.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudDebugPeriod.Name = "nudDebugPeriod";
+            this.nudDebugPeriod.Size = new System.Drawing.Size(120, 20);
+            this.nudDebugPeriod.TabIndex = 8;
+            this.nudDebugPeriod.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(401, 298);
+            this.Controls.Add(this.btnDebug);
+            this.Controls.Add(this.lblDebug);
+            this.Controls.Add(this.nudDebugPeriod);
+            this.Controls.Add(this.btnError);
+            this.Controls.Add(this.lblErrorPeriod);
+            this.Controls.Add(this.nudErrorPeriod);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.lblPeriod);
             this.Controls.Add(this.nudPeriod);
@@ -104,6 +192,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.nudPeriod)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudErrorPeriod)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDebugPeriod)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,7 +206,15 @@
         private System.Windows.Forms.NumericUpDown nudPeriod;
         private System.Windows.Forms.Label lblPeriod;
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerInfo;
+        private System.Windows.Forms.Label lblErrorPeriod;
+        private System.Windows.Forms.NumericUpDown nudErrorPeriod;
+        private System.Windows.Forms.Button btnError;
+        private System.Windows.Forms.Timer timerError;
+        private System.Windows.Forms.Button btnDebug;
+        private System.Windows.Forms.Label lblDebug;
+        private System.Windows.Forms.NumericUpDown nudDebugPeriod;
+        private System.Windows.Forms.Timer timerDebug;
     }
 }
 
--- LogGenerator/Form1.cs
+++ LogGenerator/Form1.cs
@@ -10,6 +10,9 @@ namespace LogGenerator
         public Form1()
         {
             InitializeComponent();
+            timerInfo.Tick += LogInfo;
+            timerError.Tick += LogError;
+            timerDebug.Tick += LogDebug;
         }
         StreamWriter writer;
 
@@ -17,17 +20,31 @@ namespace LogGenerator
         {
             var fileName = string.Format(tbPath.Text, DateTime.Now);
             writer = new StreamWriter(fileName);
-            timer1.Tick += LogInfo;
-            timer1.Interval = (int)nudPeriod.Value;
-            timer1.Start();
+            timerInfo.Interval = (int)nudPeriod.Value;
+            timerInfo.Start();
+            timerError.Interval = (int)nudErrorPeriod.Value;
+            timerError.Start();
+            timerDebug.Interval = (int)nudDebugPeriod.Value;
+            timerDebug.Start();
         }
         int n = 0;
         Random rand = new Random(0);
         private void LogInfo(object sender, EventArgs e)
         {
-            Log($"Request sent: {n++}");
+            Log($"Request sent: {n}");
             Thread.Sleep(rand.Next(50));
-            Log($"Reply received: {n}");
+            Log($"Reply received: {n++}");
+        }
+
+        string[] users = {"Eric", "Jimi", "Angus", "Allen", "Pete", "Joe", "Kirk" };
+        private void LogError(object sender, EventArgs e)
+        {
+            Log($"Authentication error: {users[rand.Next(users.Length-1)]}", "ERROR");
+        }
+
+        private void LogDebug(object sender, EventArgs e)
+        {
+            Log($"Memory used: {rand.Next(128)}", "DEBUG");
         }
 
         private void Log(string msg, string type="INFO")
@@ -35,7 +52,16 @@ namespace LogGenerator
             string line = $"{DateTime.Now:HH:mm:ss.fff} - {type} - {msg}";
             writer.WriteLine(line);
             writer.Flush();
+        }
 
+        private void btnError_Click(object sender, EventArgs e)
+        {
+            LogError(null, EventArgs.Empty);
+        }
+
+        private void btnDebug_Click(object sender, EventArgs e)
+        {
+            LogDebug(null, EventArgs.Empty);
         }
     }
 }
